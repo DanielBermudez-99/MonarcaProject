b318c24e177295262c868f89d03a57021e206a98
DANIEL STIVENN BERMUDEZ PRADA <136749464+DanielBermudez-99@users.noreply.github.com>
2023-10-10 11:55:01 -0500

    Inicio del proyecto Monarca

---
+++ BackEnd/BackEndMonarca/src/test/java/com/monarca/backendmonarca/BackEndMonarcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monarca.backendmonarca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndMonarcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
